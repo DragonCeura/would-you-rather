d9209bb96a21b9891c682172e17ef32237f669ea
DragonCeura <tai.drahcir@gmail.com>
2018-07-16 08:48:22 -0700

    Revert "Revert "refactor: clean up Question variable usage""
    This reverts commit 47fabfc71911dfebd44197740eb866e3bf5dca65.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom'
 
 import { formatQuestion } from '../utils/Utils';
 
 class Question extends Component {
   render() {
     const { question } = this.props;
-    
+
+    const { avatar, author, id, optionOne, optionTwo, answer } = question;
+
     return(
-      <div className='question'>
-        <div>{question.optionOne}</div>
-        <div>or</div>
-        <div>{question.optionTwo}</div>
-        {question[question.answer] ? <h5>Answered: {question[question.answer]}</h5> : null}
-      </div>
+      <Link to={`/question/${id}`} className='question'>
+        <img
+          src={avatar}
+          alt={`Avatar of ${author}`}
+          className='avatar'
+        />
+        <div className='question-info'>
+          <div>{optionOne}</div>
+          <div>or</div>
+          <div>{optionTwo}</div>
+          {answer ? <h5>Answered: {question[answer]}</h5> : null}
+        </div>
     )
   }
 }
@@ -29,4 +38,4 @@ function mapStateToProps({ authedUser, users, questions }, { question_id }) {
   }
 }
 
-export default connect(mapStateToProps)(Question);
+export default withRouter(connect(mapStateToProps)(Question));
